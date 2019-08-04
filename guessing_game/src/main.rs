08528cfb24dea6de3e7209ed1bbe68c222c034e5
devisions <marius.ileana@gmail.com>
2019-08-04 20:53:15 +0300

    Added 'quit' for exit, small cosmetics

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,37 +1,42 @@
 extern crate rand;
 
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+use std::io::Write;
 
 fn main() {
-    println!("---------- Guess the number ----------");
+    println!("\n---------- Guess the number ----------\n");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     loop {
-        println!("Enter your guessing: ");
+        print!("Enter your guessing: ");
+        std::io::stdout().flush().unwrap();
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error: Failed to read line.");
+        if guess.trim() == "quit" {
+            println!("\nGoodbye\n");
+            break;
+        }
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(err) => {
-                println!("Error: {}", err);
+            Err(_) => {
+                println!();
                 continue;
             }
         };
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Nope, it's smaller.\n"),
-            Ordering::Greater => println!("Nope, it's greater.\n"),
+            Ordering::Less => println!("Nope, it's greater.\n"),
+            Ordering::Greater => println!("Nope, it's smaller.\n"),
             Ordering::Equal => {
                 println!("Great! You guessed it!\n");
                 break;
             }
         }
-
     }
-    
 }
