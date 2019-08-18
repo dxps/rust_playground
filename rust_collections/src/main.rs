d69ab4d147aed9171e956bb134c22c2d00939eb0
devisions <marius.ileana@gmail.com>
2019-08-18 21:47:13 +0300

    hashmap & ownership

--- rust_collections/src/main.rs
+++ rust_collections/src/main.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("\n---------- Vector > getting elements ----------\n");
+    println!("\n---------- vector > getting elements ----------\n");
 
     let mut v = vec![1, 2, 3]; // using vec! macro, otherwise use Vector::new()
     v.push(4);
@@ -10,4 +12,14 @@ fn main() {
         Some(element) => println!("5th element is {}", element),
         None => println!("5th element is non-existent"),
     }
+
+    println!("\n---------- hash maps & ownership ----------\n");
+
+    let mut hm = HashMap::new();
+    let user1_uid = "uid-123"; // this is an &str, so no movement happens
+    let _user2_uid = String::from("uid-123"); // but this String would be moved
+                                              // so we won't be able to use it later
+    hm.insert(user1_uid, String::from("user-1"));
+    hm.insert("uid-456", String::from("user-2"));
+    println!("Added uid: {:?} to hm = {:?}", user1_uid, hm);
 }
