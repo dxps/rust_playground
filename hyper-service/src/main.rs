deed6d5047239276800119d704f0978129072a12
devisions <marius.ileana@gmail.com>
2020-02-09 23:19:22 +0200

    hyper-service > deps upd & async

--- hyper-service/src/main.rs
+++ hyper-service/src/main.rs
@@ -1,13 +1,24 @@
-use hyper::rt::Future;
-use hyper::service::service_fn_ok;
-use hyper::{Body, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+use std::convert::Infallible;
+use std::net::SocketAddr;
 
-fn main() {
-    let addr = ([127, 0, 0, 1], 8080).into();
-    let builder = Server::bind(&addr);
-    let server =
-        builder.serve(|| service_fn_ok(|_| Response::new(Body::from("Rust microservice"))));
+async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    println!(">>> received {:?}", req);
+    Ok(Response::new("Rust hyper service".into()))
+}
 
-    let server = server.map_err(drop);
-    hyper::rt::run(server);
+#[tokio::main]
+async fn main() {
+    // For every connection, a `Service` will handle incoming HTTP requests on that connection.
+    let service = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let server = Server::bind(&addr).serve(service);
+
+    println!(">>> Listening on http://{}", addr);
+
+    if let Err(e) = server.await {
+        eprintln!(">>> Server error: {}", e);
+    }
 }
