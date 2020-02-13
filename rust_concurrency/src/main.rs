9abfcdae0da0374a0553193857e22423bf8e56a4
devisions <marius.ileana@gmail.com>
2020-02-13 23:56:43 +0200

    rust_concurrency > thread::spawn() ex

--- rust_concurrency/src/main.rs
+++ rust_concurrency/src/main.rs
@@ -1,3 +1,18 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "[{:?}] spanning a thread now.",
+        thread::current().name().unwrap()
+    );
+
+    let join_handle = thread::spawn(|| {
+        for i in 1..4 {
+            println!("[{:?}] step #{}", thread::current().id(), i);
+            thread::sleep(Duration::new(1, 0));
+        }
+    });
+
+    join_handle.join().unwrap();
 }
