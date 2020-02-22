5bf05f461b940d0c8675e96324c81bdbeb0be3cc
devisions <marius.ileana@gmail.com>
2020-02-22 20:55:08 +0200

    rust_oop > restructuring the avg_coll module

--- rust_oop/src/avg_coll.rs
+++ rust_oop/src/avg_coll.rs
@@ -1,16 +1,46 @@
-//
-// Example of using `AveragedCollection` that is defined in `lib.rs`.
-//
+// --------------------------------------------------------------------------------------
+// `AveragedCollection` shows a some OOP-like features that Rust provides:
+// - we have a constructor
+// - there are a couple of methods
+//       - `add` and `remove` are changing the internal structure of the instance
+//       - `get_average` behaves like a getter
+//       - `update_average` is just for internal (non-public) usage
+// --------------------------------------------------------------------------------------
 
-use rust_oop::AveragedCollection;
 
-fn main() {
-    let mut coll = AveragedCollection::new();
-    coll.add(2).add(8).add(11);
-    println!(">>> Collection's average is {}", coll.get_average());
-    let item = coll.remove();
-    if item.is_some() {
-        println!(">>> Removed item {} from collection.", item.unwrap());
-        println!(">>> Collection's average is {}", coll.get_average());
+pub struct AveragedCollection {
+    list: Vec<i32>,
+    average: f64,
+}
+
+impl AveragedCollection {
+    pub fn new() -> AveragedCollection {
+        AveragedCollection {
+            list: Vec::new(),
+            average: 0 as f64,
+        }
+    }
+
+    pub fn add(&mut self, value: i32) -> &mut AveragedCollection {
+        self.list.push(value);
+        self.update_average();
+        self
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        let item = self.list.pop();
+        if item.is_some() {
+            self.update_average();
+        }
+        item
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+
+    pub fn get_average(&self) -> f64 {
+        self.average
     }
 }
---
+++ rust_oop/src/avg_coll_bin.rs
@@ -0,0 +1,16 @@
+//
+// Example of using `AveragedCollection` that is defined in `lib.rs`.
+//
+
+mod avg_coll;
+
+fn main() {
+    let mut coll = avg_coll::AveragedCollection::new();
+    coll.add(2).add(8).add(11);
+    println!(">>> Collection's average is {}", coll.get_average());
+    let item = coll.remove();
+    if item.is_some() {
+        println!(">>> Removed item {} from collection.", item.unwrap());
+        println!(">>> Collection's average is {}", coll.get_average());
+    }
+}
