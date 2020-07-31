a86ab9e1b151191864a7756be2281f72303ff73f
devisions <marius.ileana@gmail.com>
2020-07-31 22:39:22 +0300

    pipeviewer > restructure using lib modules

--- pipeviewer/src/main.rs
+++ pipeviewer/src/main.rs
@@ -1,62 +1,23 @@
-use clap::{App, Arg};
-use std::env;
-use std::fs::File;
-use std::io::{self, BufReader, BufWriter, ErrorKind, Read, Result, Write};
-
-/// 16 kb for the chunk of the bytes
-const CHUNK_SIZE: usize = 16 * 1024;
+use pipeviewer::{args::Args, read, stats, write};
+use std::io::Result;
 
 fn main() -> Result<()> {
-    let matches = App::new("pipeviewer")
-        .version("0.0.1")
-        .arg(Arg::with_name("infile").help("Read from a file instead of stdin"))
-        .arg(
-            Arg::with_name("outfile")
-                .short("o")
-                .long("outfile")
-                .takes_value(true),
-        )
-        .arg(Arg::with_name("silent").short("s").long("silent"))
-        .get_matches();
-
-    let infile = matches.value_of("infile").unwrap_or_default();
-    let outfile = matches.value_of("outfile").unwrap_or_default();
-    let silent = if matches.is_present("silent") {
-        true
-    } else {
-        !env::var("PV_SILENT").unwrap_or_default().is_empty()
-    };
-
-    let mut reader: Box<dyn Read> = if !infile.is_empty() {
-        Box::new(BufReader::new(File::open(infile)?))
-    } else {
-        Box::new(BufReader::new(io::stdin()))
-    };
-
-    let mut writer: Box<dyn Write> = if !outfile.is_empty() {
-        Box::new(BufWriter::new(File::create(outfile)?))
-    } else {
-        Box::new(BufWriter::new(io::stdout()))
-    };
-
+    let args = Args::parse();
     let mut total_bytes = 0;
-    let mut buffer = [0; CHUNK_SIZE];
     loop {
-        let num_read = match reader.read(&mut buffer) {
-            Ok(0) => break,
+        let buffer = match read::read(&args.infile) {
+            Ok(x) if x.is_empty() => break,
             Ok(x) => x,
             Err(_) => break,
         };
-        total_bytes += num_read;
-        if !silent {
-            eprint!("\r[total bytes: {}]", total_bytes);
-        }
-        if let Err(e) = writer.write_all(&buffer[..num_read]) {
-            if e.kind() == ErrorKind::BrokenPipe {
-                break;
-            }
-            return Err(e);
+
+        stats::stats(args.silent, buffer.len(), &mut total_bytes, false);
+
+        if !write::write(&args.outfile, &buffer)? {
+            break;
         }
     }
+    stats::stats(args.silent, 0, &mut total_bytes, true);
+
     Ok(())
 }
