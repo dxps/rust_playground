2df22408dfe57ad2f314e725c0e18836a1113bb4
devisions <marius.ileana@gmail.com>
2020-08-15 20:42:01 +0300

    warp_todo_crud_ex1 > applied clippy advices

--- warp_todo_crud_ex1/src/error.rs
+++ warp_todo_crud_ex1/src/error.rs
@@ -34,7 +34,10 @@ pub async fn handle_rejection(err: Rejection) -> std::result::Result<impl Reply,
     if err.is_not_found() {
         code = StatusCode::NOT_FOUND;
         message = "Not Found";
-    } else if let Some(_) = err.find::<warp::filters::body::BodyDeserializeError>() {
+    } else if err
+        .find::<warp::filters::body::BodyDeserializeError>()
+        .is_some()
+    {
         code = StatusCode::BAD_REQUEST;
         message = "Invalid Body";
     } else if let Some(e) = err.find::<Error>() {
@@ -49,7 +52,7 @@ pub async fn handle_rejection(err: Rejection) -> std::result::Result<impl Reply,
                 message = "Internal Server Error";
             }
         }
-    } else if let Some(_) = err.find::<warp::reject::MethodNotAllowed>() {
+    } else if err.find::<warp::reject::MethodNotAllowed>().is_some() {
         code = StatusCode::METHOD_NOT_ALLOWED;
         message = "Method Not Allowed";
     } else {
--- warp_todo_crud_ex1/src/handler.rs
+++ warp_todo_crud_ex1/src/handler.rs
@@ -14,9 +14,7 @@ pub struct SearchQuery {
 }
 
 pub async fn health_handler(db_pool: DbPool) -> std::result::Result<impl Reply, Rejection> {
-    let db = db::get_db_conn(&db_pool)
-        .await
-        .map_err(|e| reject::custom(e))?;
+    let db = db::get_db_conn(&db_pool).await.map_err(reject::custom)?;
 
     db.execute("SELECT 1", &[])
         .await
@@ -28,16 +26,16 @@ pub async fn create_todo_handler(body: TodoRequest, db_pool: DbPool) -> Result<i
     Ok(json(&TodoResponse::of(
         db::create_todo(&db_pool, body)
             .await
-            .map_err(|e| reject::custom(e))?,
+            .map_err(reject::custom)?,
     )))
 }
 
 pub async fn list_todos_handler(query: SearchQuery, db_pool: DbPool) -> Result<impl Reply> {
     let todos = db::fetch_todos(&db_pool, query.search)
         .await
-        .map_err(|e| reject::custom(e))?;
+        .map_err(reject::custom)?;
     Ok(json::<Vec<_>>(
-        &todos.into_iter().map(|t| TodoResponse::of(t)).collect(),
+        &todos.into_iter().map(TodoResponse::of).collect(),
     ))
 }
 
@@ -49,13 +47,13 @@ pub async fn update_todo_handler(
     Ok(json(&TodoResponse::of(
         db::update_todo(&db_pool, id, body)
             .await
-            .map_err(|e| reject::custom(e))?,
+            .map_err(reject::custom)?,
     )))
 }
 
 pub async fn delete_todo_handler(id: i32, db_pool: DbPool) -> Result<impl Reply> {
     db::delete_todo(&db_pool, id)
         .await
-        .map_err(|e| reject::custom(e))?;
+        .map_err(reject::custom)?;
     Ok(StatusCode::OK)
 }
