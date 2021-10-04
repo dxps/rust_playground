1522beb0e24fcbf40c94f2e16cb5c3ac26765429
dxps <marius.ileana@gmail.com>
2021-10-04 20:48:47 +0300

    chore(rust_concurrency): doc upd & fmt

---
+++ rust_concurrency/readme.md
@@ -0,0 +1,7 @@
+## Rust Concurrency
+
+A small playground on Rusts' concurrency and threading capabilities.
+
+To run any of the included "binaries", use `cargo run --bin {binary-name}`.<br>
+See `Cargo.toml` for their names, as being defined there.<br>
+Usage example: `cargo run --bin spawning_a_thread`
--- rust_concurrency/src/spawning_a_thread.rs
+++ rust_concurrency/src/spawning_a_thread.rs
@@ -2,18 +2,32 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+    let main_thread = thread::current();
+
     println!(
-        "[{:?}] spanning a thread now.",
-        thread::current().name().unwrap()
+        "[{:?} {:?}] spanning a thread now.",
+        main_thread.name().unwrap(),
+        main_thread.id()
     );
 
     let join_handle = thread::spawn(|| {
         for i in 1..4 {
-            println!("[{:?}] step #{}", thread::current().id(), i);
+            let curr_thread = thread::current();
+            println!(
+                "[{:?} {:?}] step #{}.",
+                curr_thread.name().unwrap_or("unnamed"),
+                curr_thread.id(),
+                i
+            );
             thread::sleep(Duration::new(1, 0));
         }
     });
 
+    println!(
+        "[{:?} {:?}] is joining the spawned thread.",
+        main_thread.name().unwrap(),
+        main_thread.id().to_owned(),
+    );
     // Waiting for the spawned thread to finish, then end our main function (and thread).
     join_handle.join().unwrap();
 }
