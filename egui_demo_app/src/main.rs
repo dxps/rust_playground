89ab7be7f9cc97d37f059b58c3419b593a325436
dxps <marius.ileana@gmail.com>
2021-10-28 19:52:59 +0300

    egui_demo_app: 1st commit

---
+++ egui_demo_app/src/main.rs
@@ -0,0 +1,15 @@
+// Forbid warnings in release builds:
+#![cfg_attr(not(debug_assertions), deny(warnings))]
+#![forbid(unsafe_code)]
+#![warn(clippy::all, rust_2018_idioms)]
+
+// When compiling natively:
+fn main() {
+    let app = egui_demo_lib::WrapApp::default();
+    let options = eframe::NativeOptions {
+        resizable: false,
+        drag_and_drop_support: true,
+        ..Default::default()
+    };
+    eframe::run_native(Box::new(app), options);
+}
