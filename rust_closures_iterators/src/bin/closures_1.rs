4aea216ac38572df84e7198c0d209a1672779d08
dxps <marius.ileana@gmail.com>
2022-05-08 23:25:14 +0300

    rust_closures_iterators: rename, ex2

--- rust_closures_iterators/src/bin/closures_1.rs
+++ rust_closures_iterators/src/bin/closures_1.rs
@@ -1,3 +1,7 @@
+///
+/// An example of declaring, passing and calling a closure.
+///
+
 fn math(a: i32, b: i32, op: Box<dyn Fn(i32, i32) -> i32>) -> i32 {
     op(a, b)
 }
@@ -8,12 +12,13 @@ fn main() {
     println!("add: {}", math(2, 2, add));
     println!("sub: {}", math(2, 2, sub));
 
-    // Closures can also take data from outside their environment.
-    // In this example, "move" must be used to "capture" the flag internally.
+    // Closures can also take data from the outside environment.
+    // In this case, "move" must be used to "capture" the flag internally.
     let flag = "dbg";
     let mul = Box::new(move |a, b| {
-        println!("[{}] on {} and {}", flag, a, b);
+        println!("{} | [mul] on {} and {}", flag, a, b);
         a * b
     });
+
     println!("mul: {}", math(2, 2, mul));
 }
---
+++ rust_closures_iterators/src/bin/ex_2.rs
@@ -0,0 +1,59 @@
+///
+/// Exercise from "Ultimate Rust 2 - Intermediate Concepts"
+///
+
+// Yes, yes, we know. It's an exercise, compiler, we want it that way!
+#[allow(unused_mut)]
+
+fn main() {
+    // 1. Uncomment the code below. Create a closure that returns the square of an integer (the
+    // number multiplied by itself), and assign the closure to the "square" variable. Then run the
+    // code and make sure it works.
+
+    let square = |n| n * n;
+    println!("5 squared is {}", square(5));
+
+    // 2. Uncomment the code below.  Finish the .map() iterator adaptor call by passing it a closure
+    // which takes a tuple of two integers as a parameter, and returns a tuple with the first
+    // integer incremented by 1, and the second integer left alone.  For example, if given the input
+    // (0, 1), it should return (1, 1). Run the code and make sure it works.
+
+    let pairs = vec![(0, 1), (2, 3), (4, 5)];
+    pairs
+        .into_iter()
+        .map(|(m, n)| (m + 1, n))
+        .for_each(|t| println!("{:?}", t));
+
+    // 3. Uncomment the code below. There is a mutable vector named `numbers`. Use an iterator over
+    // mutable references to multiply each of the values in `numbers` by 3.
+    // Hint 1: You'll need .iter_mut() -- bonus points if you use the shorter, syntactic sugar form!
+    // Hint 2: `x` will be a mutable reference, so remember to dereference it to use it
+
+    let mut numbers = vec![1, 2, 3, 4];
+    for x in &mut numbers {
+        *x *= 3 // multiply the value by 3 via the mutable reference x
+    }
+    println!("{:?}", numbers); // should print [3, 6, 9, 12]
+
+    // 4. Uncomment the code below.  Take the vector of words and
+    // - Convert the vector into an iterator with .into_iter()
+    // - Use .filter() to remove any word that contains the letter "h" -- use .contains()
+    // - Use .map() to convert all the words to uppercase -- use .to_uppercase()
+    // - Use .collect() to put the transformed words back into a vector
+    //
+    // Hint: .to_uppercase() is a method on `str` which returns a String
+
+    let words = vec!["autobot", "beach", "car", "decepticon", "energon", "frothy"];
+    let transformed: Vec<_> = words
+        .into_iter()
+        .filter(|w| !w.contains("h"))
+        .map(|w| w.to_uppercase())
+        .collect();
+    println!("Transformed: {:?}", transformed);
+
+    // Challenge:
+    //
+    // - Rewrite the code in #2 as a for loop
+    // - Rewrite the code in #3 in functional style (without a for loop).  Hint: There are multiple
+    // ways to accomplish this, but they all end with an iterator consumer.
+}
