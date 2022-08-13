a5d6f2e926ab3a54db4ad42c55cdfece5f6700bc
dxps <marius.ileana@gmail.com>
2022-08-13 16:03:46 +0300

    add yew_tutorial_videos

---
+++ yew_tutorial_videos/src/main.rs
@@ -0,0 +1,101 @@
+use yew::prelude::*;
+use yew_tutorial_videos::videos::Video;
+
+#[function_component(App)]
+fn app() -> Html {
+    let videos = vec![
+        Video {
+            id: 1,
+            title: "Building and breaking things".to_string(),
+            speaker: "John Doe".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 2,
+            title: "The development process".to_string(),
+            speaker: "Jane Smith".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 3,
+            title: "The Web 7.0".to_string(),
+            speaker: "Matt Miller".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 4,
+            title: "Mouseless development".to_string(),
+            speaker: "Tom Jerry".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+    ];
+
+    let selected_video = use_state(|| None);
+    let on_video_select = {
+        let selected_video = selected_video.clone();
+        Callback::from(move |video: Video| selected_video.set(Some(video)))
+    };
+    let details = selected_video.as_ref().map(|video| {
+        html! {
+            <VideoDetails video={video.clone()} />
+        }
+    });
+
+    html! {
+        <>
+            <h1>{ "RustConf Explorer" }</h1>
+            <div>
+                <h3>{"Videos to watch"}</h3>
+                <VideosList videos={videos} on_click={on_video_select.clone()} />
+            </div>
+            { for details }
+            <div>
+                <h3>{ "John Doe: Building and breaking things" }</h3>
+                <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
+            </div>
+        </>
+    }
+}
+
+#[derive(Clone, PartialEq, Properties)]
+struct VideosListProps {
+    videos: Vec<Video>,
+    on_click: Callback<Video>,
+}
+
+#[function_component(VideosList)]
+fn videos_list(props: &VideosListProps) -> Html {
+    props
+        .videos
+        .iter()
+        .map(|video| {
+            let on_video_click = {
+                // let on_click = props.on_click.clone();
+                // Callback::from(move |_| on_click.emit(video.clone()))
+            };
+            // <p onclick={on_video_click}>{ format!("{}: {}", video.speaker, video.title) }</p>
+            html! {
+                <p>{ format!("{}: {}", video.speaker, video.title) }</p>
+            }
+        })
+        .collect()
+}
+
+#[derive(Clone, Properties, PartialEq)]
+struct VideosDetailsProps {
+    video: Video,
+}
+
+#[function_component(VideoDetails)]
+fn video_details(VideosDetailsProps { video }: &VideosDetailsProps) -> Html {
+    html! {
+        <div>
+            <h3>{ video.title.clone() }</h3>
+            <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
+        </div>
+    }
+}
+
+fn main() {
+    yew::start_app::<App>();
+}
