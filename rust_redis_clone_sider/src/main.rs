500de337b763746fc77af2949a50c1c1b9cf74b3
dxps <marius.ileana@gmail.com>
2024-11-02 12:54:47 +0200

    feat(rust_redis_clone_sider): handle multiple clients

--- rust_redis_clone_sider/src/main.rs
+++ rust_redis_clone_sider/src/main.rs
@@ -1,17 +1,21 @@
-use std::{
-    io::{Read, Write},
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    //
     let endpoint = "127.0.0.1:6379";
-    let listener = TcpListener::bind(endpoint).expect(&format!("Failed to listen to {}.", endpoint));
+    let listener = TcpListener::bind(endpoint)
+        .await
+        .expect(&format!("Failed to listen to {}.", endpoint));
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
+    loop {
+        match listener.accept().await {
+            Ok((stream, _)) => {
                 println!("New connection from '{}'.", stream.peer_addr().unwrap());
-                handle_connection(&mut stream);
+                tokio::spawn(handle_connection(stream));
             }
             Err(e) => {
                 println!("Failed to accept connection. Cause: '{}'.", e);
@@ -20,18 +24,19 @@ fn main() {
     }
 }
 
-fn handle_connection(stream: &mut TcpStream) {
+async fn handle_connection(mut stream: TcpStream) {
     let mut buff = [0; 512];
     loop {
-        match stream.read(&mut buff) {
+        match stream.read(&mut buff).await {
             Ok(0) => {
                 println!("Connection closed.");
                 break;
             }
             Ok(_) => {
                 let response = "+PONG\r\n";
-                stream.write(response.as_bytes()).unwrap();
-                stream.flush().unwrap();
+                if let Err(e) = stream.write_all(response.as_bytes()).await {
+                    eprintln!("Failed to write to connection. Cause: '{}'.", e);
+                }
             }
             Err(e) => {
                 println!("Failed to read from connection. Cause: '{}'.", e);
