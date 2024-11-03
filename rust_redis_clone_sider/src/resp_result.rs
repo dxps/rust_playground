cee82e4858ef154025df72419f49f1f8184ed8a9
dxps <marius.ileana@gmail.com>
2024-11-03 13:21:55 +0200

    convert binary values to string

--- rust_redis_clone_sider/src/resp_result.rs
+++ rust_redis_clone_sider/src/resp_result.rs
@@ -1,15 +1,23 @@
-use std::fmt::Display;
+use std::{fmt::Display, string::FromUtf8Error};
 
 #[derive(Debug)]
 pub enum RespError {
+    FromUtf8,
     OutOfBounds(usize),
 }
 
 impl Display for RespError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            RespError::FromUtf8 => write!(f, "Cannot convert to UTF-8"),
             RespError::OutOfBounds(index) => write!(f, "Out of bounds at index {}", index),
         }
+    }
+}
+
+impl From<FromUtf8Error> for RespError {
+    fn from(_: FromUtf8Error) -> Self {
+        RespError::FromUtf8
     }
 }
 
